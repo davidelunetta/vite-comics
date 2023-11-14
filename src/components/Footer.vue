348026ded61fad8877e604bea229822bf9e8bc09
davidelunetta <davidelunetta.27@gmail.com>
2023-11-14 15:40:04 +0100

    updated footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container">
+        <div class="left">
+
+            <div class="inner">
+                <h2>DC COMICS</h2>
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+                <div class="inner-shop">
+                    <h2>SHOP</h2>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="inner">
+                <h2>DC</h2>
+                <ul>
+                    <li>Terms of use</li>
+                    <li>Privacy policy (New)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshop</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+
+            <div class="inner">
+                <h2>SITES</h2>
+                <ul>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+
+
+        </div>
+        <div class="right"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/footer.scss"
+</style>
