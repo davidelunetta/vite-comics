03e41aea2b055ffa1e625f9121dddfb46553618d
davidelunetta <davidelunetta.27@gmail.com>
2023-11-14 15:14:26 +0100

    header component updated

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <nav>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/2048px-DC_Comics_logo.svg.png" alt="">
+            <ul>
+                <li v-for="(item, index) in nav" :key="index">{{item.label}}
+                    <a :href="nav.url"></a>
+                </li> 
+            </ul>
+        </nav>
+        <div class="jumbotron">
+            <button class="series">CURRENT SERIES</button>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+  props:{
+      nav:Array,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/header.scss"
+</style>
