5ed105d77ceb059f376f21304b5b7ce3bb25852c
davidelunetta <davidelunetta.27@gmail.com>
2023-11-14 15:28:15 +0100

    updated section

---
+++ src/components/BuySection.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="blue">
+        <div class="inner-buy">
+
+            <div class="item">
+                <img src="@/assets/img/buy-comics-digital-comics.png" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+
+            <div class="item">
+                <img src="@/assets/img/buy-comics-merchandise.png" alt="">
+                <span>DC MERCHANDISE</span>
+            </div>
+
+            <div class="item">
+                <img src="@/assets/img/buy-comics-subscriptions.png" alt="">
+                <span>SUBSCRIPTION</span>
+            </div>
+
+            <div class="item">
+                <img src="@/assets/img/buy-comics-shop-locator.png" alt="">
+                <span>COMIC SHOP LOCATOR</span>
+            </div>
+
+            <div class="item">
+                <img src="@/assets/img/buy-dc-power-visa.svg" alt="">
+                <span>DC POWER VISA</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .blue{
+        height: 150px;
+        background-color: #0282F9;
+        display: flex;
+        align-items: center;
+
+        .inner-buy{
+            width: 1300px;
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
+
+            .item{
+                display: flex;
+                align-items: center;
+                color: #fff;
+                padding: 0;
+
+                img{
+                    width: 50px;
+                    margin-right: 10px;
+                }
+            }
+        }
+    }
+</style>
---
+++ src/components/SignSection.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="box">
+         <div class="sign-up">
+             <button>SIGN-UP NOW!</button>
+         </div>
+         <div class="follow-us">
+             <h2>FOLLOW US</h2>
+             <img src="@/assets/img/footer-facebook.png" alt="">
+             <img src="@/assets/img/footer-twitter.png" alt="">
+             <img src="@/assets/img/footer-youtube.png" alt="">
+             <img src="@/assets/img/footer-periscope.png" alt="">
+             <img src="@/assets/img/footer-pinterest.png" alt="">
+         </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.box{
+    display: flex;
+    justify-content: space-around;
+    height: 110px;
+    background-color: #303030;
+
+    .follow-us{
+        display: flex;
+        width: 500px;
+        align-items: center;
+        color: #076CC9;
+
+        img{
+            height: 35px;
+            margin-left: 25px;
+            cursor: pointer;
+        }
+    }
+
+    .sign-up{
+        display: flex;
+        align-items: center;
+        button{
+            border: 2px solid #037AE8;
+            padding: 20px;
+            background-color: #303030;
+            color: #fff;
+            font-weight: bold;
+            font-size: 20px;
+            width: 200px;
+            cursor: pointer;
+        }
+    }
+
+}
+
+</style>
