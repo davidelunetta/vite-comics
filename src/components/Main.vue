596bfd9ebdb78b62029a0af8ae93f69ff73c31d3
davidelunetta <davidelunetta.27@gmail.com>
2023-11-14 14:21:10 +0100

    created component

---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <main class="black">
+            <div class="container">
+                <div id="card" v-for="(items, index) in cards" :key="index">
+                    <div class="image">
+                        <img :src="items.thumb" alt="">
+                    </div>
+                    <span id="price">{{items.price}}</span>
+                    <span>{{items.series}}</span>
+                </div>
+            </div>
+            <div class="load-more">
+                <button>LOAD MORE</button>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        cards: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/main.scss"
+</style>
